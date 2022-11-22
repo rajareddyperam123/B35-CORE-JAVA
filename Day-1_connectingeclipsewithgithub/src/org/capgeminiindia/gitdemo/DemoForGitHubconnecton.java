11314792fbe0db140a0006559872e05da7d42ff3
P RAJA REDDY <P RAJA REDDY@LAPTOP-N0AS1RU2>
2022-11-22 15:12:16 +0530

    first commit

---
+++ Day-1_connectingeclipsewithgithub/src/org/capgeminiindia/gitdemo/DemoForGitHubconnecton.java
@@ -0,0 +1,12 @@
+package org.capgeminiindia.gitdemo;
+
+//demo on to connect eclipse with GitHub
+
+/*..1.create a class and print a statement..*/
+public class DemoForGitHubconnecton {
+ static void main(String[] args) {
+		System.out.println("welcom to capgemini india foundation");
+
+	}
+
+}
