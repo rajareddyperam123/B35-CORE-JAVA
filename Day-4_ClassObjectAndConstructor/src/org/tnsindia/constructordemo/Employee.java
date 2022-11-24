140d2072de581867171f79eb45c4c4e9ec619c16
P RAJA REDDY <P RAJA REDDY@LAPTOP-N0AS1RU2>
2022-11-24 13:56:04 +0530

    first commit

---
+++ Day-4_ClassObjectAndConstructor/src/org/tnsindia/constructordemo/Employee.java
@@ -0,0 +1,23 @@
+package org.tnsindia.constructordemo;
+//drive class
+public class Employee {
+
+public static void main(String[] args) {
+		
+		
+		//object creation
+		Employee e=new Employee();
+		Employee e1=new Employee();
+		Employee e2=new Employee();
+
+		e1.print();
+		e2.print();
+
+	}
+
+private void print() {
+	// TODO Auto-generated method stub
+	
+}
+
+}
