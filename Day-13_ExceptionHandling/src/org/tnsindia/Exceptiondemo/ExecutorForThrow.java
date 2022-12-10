b54baddf028a32d105ca4d36d29102977ce6e061
P RAJA REDDY <P RAJA REDDY@LAPTOP-N0AS1RU2>
2022-12-10 14:01:37 +0530

    first commit

---
+++ Day-13_ExceptionHandling/src/org/tnsindia/Exceptiondemo/ExecutorForThrow.java
@@ -0,0 +1,11 @@
+package org.tnsindia.Exceptiondemo;
+//driver class
+public class ExecutorForThrow {
+
+	public static void main(String[] args) {
+		DemoOnThrowkeyword d=new DemoOnThrowkeyword();
+		d.display("Welcome to TNS India");
+
+	}
+
+}
